dd411fbe14b3bbe945809d7433eef3c23d870b18
Markus Jämbäck <markus.jamback@tuni.fi>
2021-04-11 11:31:39 +0300

    Add DataSource interface

--- softwareDesingProject/idatasource.h
+++ softwareDesingProject/idatasource.h
@@ -16,8 +16,14 @@ class IDataSource : public QObject
 {
     Q_OBJECT
 public:
-    explicit IDataSource(QObject *parent = nullptr) : QObject(parent) {};
-    virtual void load(QUrl url) = 0;
+    explicit IDataSource(QObject *parent = nullptr) : QObject(parent) {}
+
+    virtual void setLocation(QString location) = 0;
+    virtual void setTimeWindow(QString startTime, QString endTime) = 0;
+    virtual void makeRequest() = 0;
+
+    // Sitten vielä mahdollisesti jotain metodeja tuon piirakkamallin
+    // toteutukseen.
 
 signals:
     void dataParsed(DataContainer* data);
