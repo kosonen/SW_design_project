5588f26678cc5a355ed46f7280a772fb3e35138d
Kari Hepola <kari.hepola@tuni.fi>
2021-04-17 13:28:26 +0300

    Add interace for fetching pie model data from fingrid api

--- softwareDesingProject/fingriddatasource.h
+++ softwareDesingProject/fingriddatasource.h
@@ -14,20 +14,28 @@ public:
     virtual void setTimeWindow(QString startTime, QString endTime);
     virtual void makeRequest();
     virtual void setSearchParameter(const QString param);
+    void getCurrentProduction();
+
+signals:
+    void currentProductionReady(DataContainer* data);
 
 private Q_SLOTS:
     void downloadCompleted(QNetworkReply* reply);
+    void pieModelDownloadCompleted(QNetworkReply* reply);
 
 private:
 
     QUrl buildFingridURL(bool forecast, QString startTime, QString endTime);
+    QUrl buildFingridURL(QString source);
+    void parseData(QDomDocument& doc, QList<QPointF>& buffer);
+    void parsePieModelData(QDomDocument& doc, QList<QPointF>& buffer);
+    void pieModelFetchHandler();
     void fetchHandler();
 
 
 
     QNetworkAccessManager* network_;
-    QXmlStreamReader* parser_;
-    QHash<QString,QString> queryParameters_;
+    QNetworkAccessManager* pieModelNetwork_;
 
     QString source_;
     QDateTime startTime_;
@@ -35,6 +43,11 @@ private:
 
     QList<QPointF> dataBuffer_;
     bool previousFetchesHandled_;
+
+    bool fetchingPieModel_;
+    int numberOfFetches_;
+    QList<QPointF> pieModelDataBuffer_;
+
 
 };
 
