4396fa14a6b186e1741727001e79ab9d340cf9d8
Antti Nurmi <antti.nurmi@tuni.fi>
2021-04-18 15:01:23 +0300

    commented controller.h

--- softwareDesingProject/controller.h
+++ softwareDesingProject/controller.h
@@ -24,31 +24,85 @@ class Controller : public QObject
     Q_OBJECT
 
 public:
+
+    /**
+     * @brief constructor for Controller class
+     * @param parent = nullptr in QObject parent system
+     */
     explicit Controller(QObject *parent = nullptr);
 
-    /* Called from QML when all data request configurations have been set*/
+    /**
+     * @brief Called from QML, requests Data from API after all data request
+     * configurations have been set
+     * @return true if operation successful
+     */
     Q_INVOKABLE bool requestData();
 
-    /* Sets location for the data sources to be fetched, eg. Tampere */
+    /**
+     * @brief sets location for the data sources to be fetched, eg. Tampere
+     * @param value: new location as QString
+     */
     Q_INVOKABLE void setLocation(QString value);
 
-    /* Complete list of all desires data sources,
-     * eg. {wind, solar, temperature} */
+    /**
+     * @brief sets complete list of all desires
+     * data sources, eg. {wind, solar, temperature}
+     * @param sources: list of sources in QVector
+     */
     Q_INVOKABLE void setDataSources(QVector<QString> sources);
 
-    /* All fetched data sources are fit into the same time frame */
+    /**
+     * @brief Sets time window
+     * All fetched data sources are fit into the same time frame
+     * @param startTime: new start time
+     * @param endTime: new end time
+     */
     Q_INVOKABLE void setTimeWindow(QString startTime, QString endTime);
 
+    /**
+     * @brief enables automatic update timer
+     * @param status: true to set timer
+     */
     Q_INVOKABLE void setAutomaticUpdate(bool status);
 
+    /**
+     * @brief loads Data from given path
+     * @param filePath: path to load file
+     * @return true if operation is succesful
+     */
     Q_INVOKABLE bool loadData(QString filePath);
+
+    /**
+     * @brief save Data to given path
+     * @param filePath: path to save file
+     * @param dataSource: source name to save
+     * @return true if operation is successful
+     */
     Q_INVOKABLE bool saveData(QString filePath, QString dataSource);
 
+    /**
+     * @brief sets Data Processing method
+     * eg. MIN, MAX, AVG, NONE
+     * @param method: method to set
+     */
     Q_INVOKABLE void setDataProcessing(QString method);
 
+    /**
+     * @brief get Popup Error string
+     * @return Error String
+     */
     Q_INVOKABLE QString getPopupError();
 
+    /**
+     * @brief sets Model object to control
+     * @param model: Model object pointer
+     */
     void setModel(Model* model);
+
+    /**
+     * @brief sets Settings object to use
+     * @param settings: Settings object pointer
+     */
     void setSettings(Settings* settings);
 
 Q_SIGNALS:
